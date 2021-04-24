f3fec08c49b40999d7dc0f554de9c57dfffa7ac8
Ativut Son <nazareth_0@hotmail.com>
2021-04-24 11:29:43 -0400

    Commit 4: Renamed the column

---
+++ db/migrate/20210424151342_create_character.rb
@@ -0,0 +1,11 @@
+class CreateCharacter < ActiveRecord::Migration[6.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :desciption
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210424152703_fix_description_column_typo.rb
@@ -0,0 +1,6 @@
+class FixDescriptionColumnTypo < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :characters, :desciption, :description
+
+  end
+end
